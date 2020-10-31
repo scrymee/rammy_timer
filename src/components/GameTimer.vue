84e7bf5c23e67e64b0085ef6e047c6962732227e
scrymee <syoumee@gmail.com>
2020-10-31 23:52:02 +0900

    デザインを修正

--- src/components/GameTimer.vue
+++ src/components/GameTimer.vue
@@ -1,14 +1,10 @@
 <template>
 	<div class="content">
-				{{this.time}}
-	<div class="timer" style="background:red;">
-	<section class="main-contents">
-		<section class="circle-box">
+		<div class="circle-box">
 			<div v-for="n in 60" v-bind:id="['item'+n]" class='item'>{{n}}</div>
-			<div class="item_center"v-on:click="toggleClock"></div>
-		</section>
-	</section>
-	</div>
+			<!-- <div class="item_center" v-on:click="toggleClock"></div> -->
+			<img src="../img/idol.png" v-on:click="toggleClock" class="item_center">
+		</div>
 </div>
 </template>
 
@@ -16,7 +12,6 @@
 	var default_time = 60;
 	export default {
   name: 'GameTimer',
-	//data()の中身は、Viewに返す値
   data () {
     return {
 			time: default_time,
@@ -26,9 +21,6 @@
 	methods:{
 		toggleClock: function(){
 			this.resetClock();
-			this.startClock();
-		},
-		startClock: function(){
 			this.clockMethod = setInterval(()=>this.viewTimer(), 1000);
 		},
 		resetClock: function(){
@@ -42,82 +34,66 @@
 
 			$('#item'+timer).addClass('appear');
 			$('#item'+(timer+1)).removeClass('appear');
-			if(this.time == 0) this.stopClock();
+			if(this.time == 0) this.resetClock();
 		},
 	},
 	computed:{
 	},
   created:function(){
-			console.log('hoge');
 	},
 }
 
 
-
+$(document).ready(function(){
+  $(window).mousemove(function(){
+			//console.log(event.clientX);
+			//console.log(event.clientY);
+			//206,328
+  });
+});
 
 $(function(){
+		const popup = $('div.circle-box');
     var item_num = $('div.item').length;
 		//円周角
     var deg = 360.0/item_num;
 		//半円で考える
-    var deg = deg / 2;
+    //var deg = deg / 2;
 		//ラジアンにする
-    var radian = (deg*Math.PI/180.0);
-		var radian = radian * (-1);
+    var radian = deg * (Math.PI/180.0);
+		//var radian = radian * (-1) / 2;
 		//半径
-    var rad = 250;
+    var rad = $(window).width() / 2.5;
 		//中央の点の座標
-		var centerX = rad;
-		var centerY = rad;
+		//var centerX = parent.offset().left + ( parent.width() / 2 );
+		//var centerY = parent.offset().top + ( parent.height() / 2 );
+		var centerX = $(window).width() / 2 -16;
+		var centerY = $(window).height() / 2 -16;
+		console.log(centerX);
+		console.log(centerY);
     $('div.item').each(function(i, elem) {
-        var x = centerX + Math.cos(radian * i) * rad ;
-        var y = centerY + Math.sin(radian * i) * rad ;
+        var x = centerX + Math.cos(radian * i) * rad;
+        var y = centerY + Math.sin(radian * i) * rad;
+		//console.log('id:'+i+', x;'+x);
+		//console.log('id:'+i+', rad;'+radian*i);
+		$("img.item_center").bind("load",function(){
+			const img_height = $(this).height();
+			const img_width = $(this).width();
         $(elem).css('left', x);
         $(elem).css('top', y);
+				const item_centerX = centerX - (img_width/ 2) +16;
+				const item_centerY = centerY - (img_height / 2);
+        $('.item_center').css('width', 300);
+        $('.item_center').css('left', item_centerX);
+        $('.item_center').css('bottom', item_centerY);
+		});
     });
-		$('div.item_center').css('left',rad);
-		$('div.item_center').css('top',rad);
-		$('div.item_center').css('margin-left',-50);
-		$('div.item_center').css('margin-top',-150);
 
 });
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.hello {
-	--square :calc(100vmin - 10vmin);
-	width: var(--square);
-	height: var(--square);
-	top: 0;
-	bottom: 0;
-	right: 0;
-	left: 0;
-	margin: auto;
-	background-color:#FFD580;
-	line-height:var(--square);
-}
-
-
-.clock{
-	font-size:100px;
-	color:#fff;
-	font-family:sans-serif;
-
-}
-
-
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
 a {
   color: #42b983;
 }
@@ -127,17 +103,39 @@ a {
 	transform:rotateY(360deg);
 }
 
-.main-contents {
-    position: relative;
-    width: 100%;
+#idol{
+	width : 60vh;
+	height : auto;
 }
+
+.circle2{
+    width: 80vh;
+    height: 80vh;
+		/*heightとline-heightを一致させると中央*/
+		line-height:80vh;
+
+		font-size:5vw;
+		color:white;
+		font-family:"Arial Black";
+
+    -webkit-border-radius: 50%;
+    -moz-border-radius: 50%;
+    border-radius: 50%;
+		border: 50px solid #FEAE00;
+}
+
 .circle-box {
-    position: relative;
+	position:relative;
     margin: 0 auto;
+		background-image:url('../img/background.png');
+		height:100vh;
 }
 div.item {
     position: absolute;
+		/*
 		display:none;
+		*/
+		display:block;
     width: 10vw;
     height: 10vw;
 		/*heightとline-heightを一致させると中央*/
@@ -152,9 +150,10 @@ div.item {
     border-radius: 50%;
     background-color: #FEAE00;
 }
-div#item40{
+div#item1{
 	display:block;
 }
+/*
 .item:after{
   content: "";
   position: absolute;
@@ -169,22 +168,15 @@ div#item40{
   border-width: 3vw 20px 0 20px;
 	transform: rotate(75deg);
 }
+*/
 div.appear{
 	display:block!important;
 
 }
-div.item_center {
-    position: absolute;
+.item_center {
+	position: absolute;
     width: 200px;
-    height: 300px;
-    -webkit-border-radius: 20px;
-    -moz-border-radius: 20px;
-    border-radius: 20px;
-		background-image:url('../img/idol.png');
-		background-size: contain;
-		background-repeat:  no-repeat; 
-  animation: img_box_9955 3s linear infinite;
-  transform-origin: 50% 50%;
+    height: auto;
 }
  
 @keyframes img_box_9955 {
@@ -198,4 +190,16 @@ div.spin{
 	transition:1s;
 
 }
+		.float{
+		animation: img_box_9955 3s linear infinite;
+		transform-origin: 50% 50%;
+		}
+		div.content{
+		background-image:url('../img/background.png');
+		}
+
+		body{
+		}
+
+
 </style>
