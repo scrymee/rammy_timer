a92572e9113f7c7d075d6ca14fdffe4aedd00b8a
scrymee <syoumee@gmail.com>
2020-11-01 14:22:37 +0900

    クリックによって回転するように変更

--- src/components/GameTimer_before.vue
+++ src/components/GameTimer_before.vue
@@ -1,90 +1,11 @@
-<style>
-#photos {
-   /* Prevent vertical gaps */
-   line-height: 0;
- 
-   -webkit-column-count: 4;
-   -webkit-column-gap:   0px;
-   -moz-column-count:    4;
-   -moz-column-gap:      0px;
-   column-count:         4;
-   column-gap:           0px;
- 
-}
-#photos div {
-  /* Just in case there are inline attributes */
-  /*width: 100% !important;*/
-  height: auto !important;
-}
-div.fukidashi{
-	display:inline-block;
-	position:relative;
-	--height: 15vw ;
-	width: 20vw;
-	height: var(--height);
-	line-height:var(--height);
-
-	font-size:5vw;
-	color:white;
-	font-family:"Arial Black";
-
-	-webkit-border-radius: 50%;
-	-moz-border-radius: 50%;
-	border-radius: 50%;
-	background-color: #FEAE00;
-}
-div.fukidashi:after{
-  content: "";
-  position: absolute;
-  width: 0px;
-  height: 0px;
-  margin: auto;
-  border-style: solid;
-  border-color: #FEAE00 transparent transparent transparent;
-  border-width: 3vw 20px 0 20px;
-}
-div.fukidashi1:after{
-  right: 0;
-  bottom: 0px;
-  left: 10vw;
-	transform: rotate(75deg);
-}
-div.fukidashi2:after{
-  right: 0;
-  bottom: 0px;
-  left: 5vw;
-	transform: rotate(95deg);
-	z-index:1;
-}
-div.fukidashi3:after{
-  right: 0;
-  bottom: -1vw;
-  left: -5vw;
-	transform: rotate(125deg);
-}
-div.fukidashi4:after{
-  right: 0;
-  bottom: 1vw;
-  left: -10vw;
-	transform: rotate(175deg);
-}
-</style>
 <template>
-	<div class="content" v-on:click="toggleClock">
-		<div class="none">
-			{{this.time}}
-			{{this.isStart}}
-			<div class="fukidashi_area" id="photos">
-				<div class="fukidashi fukidashi1" style="margin-top:30px;">50</div>
-				<div class="fukidashi fukidashi2" style="margin-top:80px;">48</div>
-				<div class="fukidashi fukidashi3" style="margin-top:20px;">42</div>
-				<div class="fukidashi fukidashi4" style="margin-top:40px;">37</div>
-			</div>
-			<div class="none">
-				<img src="../img/idol.png" class="idol" id="idol">
-			</div>
+	<div class="content">
+		<div class="circle-box">
+			<div v-for="n in 60" v-bind:id="['item'+n]" class='item'>{{n}}</div>
+			<!-- <div class="item_center" v-on:click="toggleClock"></div> -->
+			<img src="../img/idol.png" v-on:click="toggleClock" class="item_center">
 		</div>
-	</div>
+</div>
 </template>
 
 <script>
@@ -95,14 +16,10 @@ div.fukidashi4:after{
     return {
 			time: default_time,
 			clockMethod:null,
-			isStart: false
     }
   },
 	methods:{
 		toggleClock: function(){
-		if(!this.isStart) this.isStart = true;
-			//if(this.isStart) $('#idol').css({'transform':'rotateY(-360deg)'});
-			$('#idol').toggleClass('spin');
 			this.resetClock();
 			this.clockMethod = setInterval(()=>this.viewTimer(), 1000);
 		},
@@ -114,8 +31,6 @@ div.fukidashi4:after{
 		viewTimer: function(){
 			const timer = this.time;
 			this.time = timer-1;
-
-			//$('#idol').css({'transform':''});
 
 			$('#item'+timer).addClass('appear');
 			$('#item'+(timer+1)).removeClass('appear');
@@ -137,6 +52,44 @@ $(document).ready(function(){
   });
 });
 
+$(function(){
+		const popup = $('div.circle-box');
+    var item_num = $('div.item').length;
+		//円周角
+    var deg = 360.0/item_num;
+		//半円で考える
+    //var deg = deg / 2;
+		//ラジアンにする
+    var radian = deg * (Math.PI/180.0);
+		//var radian = radian * (-1) / 2;
+		//半径
+    var rad = $(window).width() / 2.5;
+		//中央の点の座標
+		//var centerX = parent.offset().left + ( parent.width() / 2 );
+		//var centerY = parent.offset().top + ( parent.height() / 2 );
+		var centerX = $(window).width() / 2 -16;
+		var centerY = $(window).height() / 2 -16;
+		console.log(centerX);
+		console.log(centerY);
+    $('div.item').each(function(i, elem) {
+        var x = centerX + Math.cos(radian * i) * rad;
+        var y = centerY + Math.sin(radian * i) * rad;
+		//console.log('id:'+i+', x;'+x);
+		//console.log('id:'+i+', rad;'+radian*i);
+		$("img.item_center").bind("load",function(){
+			const img_height = $(this).height();
+			const img_width = $(this).width();
+        $(elem).css('left', x);
+        $(elem).css('top', y);
+				const item_centerX = centerX - (img_width/ 2) +16;
+				const item_centerY = centerY - (img_height / 2);
+        $('.item_center').css('width', 300);
+        $('.item_center').css('left', item_centerX);
+        $('.item_center').css('bottom', item_centerY);
+		});
+    });
+
+});
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -147,21 +100,17 @@ a {
 
 .idol{
 	transition:1s;
-	/*transform:rotateY(360deg)*/;
-}
-.spin{
-	transform:rotateY(-360deg);
+	transform:rotateY(360deg);
 }
 
 #idol{
-	width : 50vh;
+	width : 60vh;
 	height : auto;
 }
 
-.circle{
-	display:inline-block;
-    width: 60vh;
-    height: 60vh;
+.circle2{
+    width: 80vh;
+    height: 80vh;
 		/*heightとline-heightを一致させると中央*/
 		line-height:80vh;
 
@@ -172,17 +121,15 @@ a {
     -webkit-border-radius: 50%;
     -moz-border-radius: 50%;
     border-radius: 50%;
-		border: 20px solid #FEAE00;
+		border: 50px solid #FEAE00;
 }
 
-.content {
+.circle-box {
 	position:relative;
     margin: 0 auto;
 		background-image:url('../img/background.png');
 		height:100vh;
 }
-
-
 div.item {
     position: absolute;
 		/*
@@ -226,12 +173,22 @@ div.appear{
 	display:block!important;
 
 }
+.item_center {
+	position: absolute;
+    width: 200px;
+    height: auto;
+}
  
 @keyframes img_box_9955 {
   0% { transform: translateY(0) }
   33.33333% { transform: translateY(-10px) }
   66.66667% { transform: translateY(0) }
   100% { transform: translateY(0) }
+}
+div.spin{
+	transform:rotateY(360deg);
+	transition:1s;
+
 }
 		.float{
 		animation: img_box_9955 3s linear infinite;
